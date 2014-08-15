fadbf1bd253ab618581375d28d8d69f282c71d28
Christian Kaenzig <christian.kaenzig@camptocamp.com>
2014-08-15 15:55:17 +0200

    Add support for duplicity backups to Amazon S3

---
+++ manifests/duplicity_s3.pp
@@ -0,0 +1,31 @@
+class backup::duplicity_s3 (
+  $bucket_name,
+  $access_key,
+  $secret_key,
+  $gpg_key_id,
+  $gpg_key_pass,
+  $gpg_secring_source,
+  $items,
+) {
+
+  include ::duplicity
+  ensure_packages(['python-boto'])
+
+  file {'/root/.gnupg':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0700',
+  }
+
+  file {'/root/.gnupg/secring.gpg':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0600',
+    source => $gpg_secring_source,
+  }
+
+  create_resources ('backup::duplicity_s3_item', $items)
+
+}
---
+++ manifests/duplicity_s3_item.pp
@@ -0,0 +1,15 @@
+define backup::duplicity_s3_item (
+  $source,
+  $ensure = present,
+) {
+
+  duplicity::backup {$name:
+    ensure      => $ensure,
+    source      => $source,
+    rules       => ['+ **'],
+    destination => "s3+http://${::backup::duplicity_s3::bucket_name}/${name}",
+    args        => '--s3-european-buckets --s3-use-new-style',
+    env_var     => ["AWS_ACCESS_KEY_ID='${::backup::duplicity_s3::access_key}'", "AWS_SECRET_ACCESS_KEY='${::backup::duplicity_s3::secret_key}'", "GPG_KEY=${::backup::duplicity_s3::gpg_key_id}", "PASSPHRASE=${::backup::duplicity_s3::gpg_key_pass}"],
+  }
+
+}
