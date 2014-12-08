13d3a1e573d8fd6dd4e5335ae22a4a00ef5016de
Mathieu Bornoz <mathieu.bornoz@camptocamp.com>
2014-12-08 17:12:00 +0100

    rspec: set default parameters

--- spec/classes/backup_mysql_spec.rb
+++ spec/classes/backup_mysql_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 describe 'backup::mysql' do
-  let(:facts) { {
+  let(:facts) {{
     :osfamily => 'Debian',
-  } }
+  }}
+
+  let(:params) do
+    {
+    :data_dir   => '/var/lib/mysql',
+    :backup_dir => '/var/backups/mysql',
+    }
+  end
+
   it { should compile.with_all_deps }
 end
