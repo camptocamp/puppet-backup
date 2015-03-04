3f98fedfe7ab146028d7f5e14458ed812577c318
Mickaël Canévet <mickael.canevet@camptocamp.com>
2015-03-04 12:22:57 +0100

    Use $::mysql::server::options['mysqld']['datadir']

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -18,7 +18,7 @@
 #   day of the week.
 #
 class backup::mysql(
-  $data_dir               = pick($::mysql::server::data_dir, '/var/lib/mysql'),
+  $data_dir               = $::mysql::server::options['mysqld']['datadir'],
   $backup_dir             = pick($::mysql::server::backup_dir, '/var/backups/mysql'),
   $cron_hour              = '2',
   $cron_minute            = '0',
