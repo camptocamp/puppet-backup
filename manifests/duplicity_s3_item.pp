59a72aec20d7b7a91c5d9eff08359625c68756b1
Simon Séhier <simon.sehier@camptocamp.com>
2017-01-19 10:21:59 +0100

    indent for readability

--- manifests/duplicity_s3_item.pp
+++ manifests/duplicity_s3_item.pp
@@ -10,7 +10,9 @@ define backup::duplicity_s3_item (
     rules       => $rules,
     destination => "s3+http://${::backup::duplicity_s3::bucket_name}/${name}",
     args        => '--s3-european-buckets --s3-use-new-style',
-    env_var     => ["AWS_ACCESS_KEY_ID='${::backup::duplicity_s3::access_key}'", "AWS_SECRET_ACCESS_KEY='${::backup::duplicity_s3::secret_key}'", "GPG_KEY=${::backup::duplicity_s3::gpg_key_id}", "PASSPHRASE=${::backup::duplicity_s3::gpg_key_pass}"],
+    env_var     => ["AWS_ACCESS_KEY_ID='${::backup::duplicity_s3::access_key}'",
+                    "AWS_SECRET_ACCESS_KEY='${::backup::duplicity_s3::secret_key}'",
+                    "GPG_KEY=${::backup::duplicity_s3::gpg_key_id}",
+                    "PASSPHRASE=${::backup::duplicity_s3::gpg_key_pass}"],
   }
-
 }
