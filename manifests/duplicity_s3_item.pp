9d5476871bca1f2b7573ba5df6b49fa5d60efc36
Simon Séhier <simon.sehier@camptocamp.com>
2017-01-26 16:01:09 +0100

    use s3 bucket_name as duplicity destination if it is an url (allow setting exoscale sos endpoint)

--- manifests/duplicity_s3_item.pp
+++ manifests/duplicity_s3_item.pp
@@ -4,11 +4,16 @@ define backup::duplicity_s3_item (
   $rules  = ['+ **'],
 ) {
 
+  $destination = $::backup::duplicity_s3::bucket_name ? {
+    /(^[a-z0-9\+]+:\/\/.*$)/ => $::backup::duplicity_s3::bucket_name,
+    default               => "s3+http://${::backup::duplicity_s3::bucket_name}/${name}",
+  }
+
   duplicity::backup {$name:
     ensure      => $ensure,
     source      => $source,
     rules       => $rules,
-    destination => "s3+http://${::backup::duplicity_s3::bucket_name}/${name}",
+    destination => $destination,
     args        => '--s3-european-buckets --s3-use-new-style',
     env_var     => ["AWS_ACCESS_KEY_ID='${::backup::duplicity_s3::access_key}'",
                     "AWS_SECRET_ACCESS_KEY='${::backup::duplicity_s3::secret_key}'",
