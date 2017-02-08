camptocamp/puppet-backup

commit 9d5476871bca1f2b7573ba5df6b49fa5d60efc36

use s3 bucket_name as duplicity destination if it is an url (allow setting exoscale sos endpoint)

manifests/duplicity_s3_item.pp
@@ -4,11 +4,16 @@ define backup::duplicity_s3_item (
   $rules  = ['+ **'],
 ) {
 
+  $destination = $::backup::duplicity_s3::bucket_name ? {
+    /(^[a-z0-9\+]+:\/\/.*$)/ => $::backup::duplicity_s3::bucket_name,
+    default               => "s3+http://${::backup::duplicity_s3::bucket_name}/${name}",
+  }
+
   duplicity::backup {$name:
     ensure      => $ensure,
     source      => $source,
     rules       => $rules,
-    destination => "s3+http://${::backup::duplicity_s3::bucket_name}/${name}",
+    destination => $destination,
     args        => '--s3-european-buckets --s3-use-new-style',
     env_var     => ["AWS_ACCESS_KEY_ID='${::backup::duplicity_s3::access_key}'",
                     "AWS_SECRET_ACCESS_KEY='${::backup::duplicity_s3::secret_key}'",

commit 2c3ad00686580e2471e9cac02baf49620c2a1c4e

put back $name behind bucket URL. fix bug introduced by 9d54768

manifests/duplicity_s3_item.pp
@@ -5,7 +5,7 @@ define backup::duplicity_s3_item (
 ) {
 
   $destination = $::backup::duplicity_s3::bucket_name ? {
-    /(^[a-z0-9\+]+:\/\/.*$)/ => $::backup::duplicity_s3::bucket_name,
+    /(^[a-z0-9\+]+:\/\/.*$)/ => "${::backup::duplicity_s3::bucket_name}/${name}",
     default               => "s3+http://${::backup::duplicity_s3::bucket_name}/${name}",
   }
 