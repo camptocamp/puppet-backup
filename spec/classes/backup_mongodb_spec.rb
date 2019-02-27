0d6a7a786614ad71e518048a3223b9acbe485496
Julien Godin <julien.godin@camptocamp.com>
2019-02-27 13:23:12 +0100

    Add tests for mongodb

---
+++ spec/classes/backup_mongodb_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'backup::mongodb' do
+  on_supported_os.each do |os, facts|
+    let :params do
+      {
+        :mongo_admin_password => 'foobar'
+      }
+    end
+    context "on #{os}" do
+      it { is_expected.to compile.with_all_deps }
+      it {
+        is_expected.to contain_file('/usr/local/bin/mongodb-backup.sh').with_content(%r{/usr/bin/mongodump .* 'foobar' .*})
+      }
+    end
+  end
+end
